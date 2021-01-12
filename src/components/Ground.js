4637847dde2359db90f4ab1ffa632a49a1a8f779
Ben Sparks <bensparks0@gmail.com>
2021-01-12 13:51:15 +0200

    Added basic Ground object

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { usePlane } from 'use-cannon'
+import { TextureLoader, RepeatWrapping } from 'three'
 
-export const Ground = () => {
+import grass from '../images/grass.jpg'
+
+export const Ground = (props) => {
+    const [ref] = usePlane(() => ({
+        rotation: [-Math.PI / 2, 0, 0],
+        ...props
+    }))
+    const texture = new TextureLoader().load(grass)
+    texture.wrapS = RepeatWrapping
+    texture.wrapT = RepeatWrapping
+    texture.repeat.set(100, 100)
+
     return (
-        <div>
-            
-        </div>
+        <mesh ref={ref} receiveShadow>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            <meshLambertMaterial attach='material' map={texture} />
+        </mesh>
     )
 }
